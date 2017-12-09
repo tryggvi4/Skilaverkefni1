b63d738d7820f5175baec5b90318d2707ee9d2cc
Tryggvi Bragason <tryggvib15@ru.is>
2017-12-09 05:03:48 +0000

    .gitignore

---
+++ Droid/Resources/layout/MovieInput.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MovieSearchB.Droid.Resources.layout
+{
+    [Activity(Label = "MovieInput")]
+    public class MovieInput : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
